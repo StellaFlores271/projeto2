a62a093ab017b5b2f7e92d2900876d7b0c434c49
StellaFlores271 <stellapereiraflores271@gmail.com>
2019-04-23 00:46:10 -0300

    Aulas 3-6: tela vertical, followtouch e impulsionando player com moedas

---
+++ Assets/scripts/FollowTouch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTouch : MonoBehaviour
+{
+    public float lerpRate = 4f;
+    void Update()
+    {
+       if (Input.GetButton("Fire1"));
+       {
+           Vector3 aux = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+           Vector3 initialPosition = transform.position;
+           Vector3 finalPosition = new Vector3(aux.x, transform.position.y, 0);
+           transform.position = Vector3.Lerp(initialPosition, finalPosition, lerpRate * Time.deltaTime);
+       } 
+    }
+}
--- Assets/scripts/PlayerControl.cs
+++ Assets/scripts/PlayerControl.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
-{   
-
-       void OnTriggerEnter2D(Collider2D other){
+{ 
+           Rigidbody2D rb;
+           void Start()
+           {
+               rb = GetComponent<Rigidbody2D>();
+           }
+           void OnTriggerEnter2D(Collider2D other){
            if (other.gameObject.CompareTag("Coin"))
            {
                SFXManager.instance.ShowCoinParticles(other.gameObject);
                AudioManager.instance.PlaySoundCoinPickup(other.gameObject);
                Destroy(other.gameObject);
                LevelManager.instance.IncrementCoinCount();
+               Impulse(10);
            }
            if (other.gameObject.CompareTag("Gift"))
            {
@@ -37,6 +42,10 @@ public class PlayerControl : MonoBehaviour
                SFXManager.instance.ShowDieParticles(gameObject);
                Destroy(gameObject);
                LevelManager.instance.ShowGameOverPanel();
+           }
+           void Impulse(float force){
+               rb.velocity = Vector3.zero;
+               rb.AddForce(Vector3.up * force, ForceMode2D.Impulse);
            }
        }
 }
